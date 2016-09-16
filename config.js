d16ec3c39cb96faa6792884b1ad12bfc83ac1d93
unknown <alexandre.istratov@AIS-PC.EfficyLounge.local>
2016-09-16 15:46:21 +0200

    Config for Efficy

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
+var config = {};
+
+config.slack_api_token = 'xoxb-80456344403-mMUnztfYQ0ybWoCOIGDh1MHq';
+
+config.regexes = [
+  { regex: /DVL-[0-9]+/ig, message: ['Open this Devel in Efficy: <https://ivm.efficy.com/efficy.dll/dialog?page=tsvn.htm&reference=[0]>'] },
+  { regex: /Devel ([0-9]+)/ig, message: ['Open this Devel in Efficy: <https://ivm.efficy.com/efficy.dll/dialog?page=tsvn.htm&reference=DVL-[0]>'] },
+//  { regex: /[A-Z]+-[0-9]+/g, message: 'http://my-jira.com/?q=[0]' },
+  { regex: /CFT-[0-9]+/ig, message: 'Open this Case in Efficy: <https://ivm.efficy.com/efficy.dll/dialog?page=tsvn.htm&reference=[0]>' },
+  { regex: /Case ([0-9]+)/ig, message: 'Open this Devel in Efficy: <https://ivm.efficy.com/efficy.dll/dialog?page=tsvn.htm&reference=CFT-[0]>' }
+];
+
+module.exports = config;
 
